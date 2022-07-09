8f89eb46221f7636e748faa2d38430adf76af53b
daiphanphuoc <daiphanphuoc@gmail.com>
2022-07-09 09:40:25 +0700

    inheritance && polymorphism

---
+++ case_study/src/models/Employee.java
@@ -0,0 +1,8 @@
+package models;
+
+public class Employee extends Person{
+    String iDStaff;
+    String degree;
+    String position;
+    double  salary;
+}
---
+++ case_study/src/models/Person.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.Date;
+
+public class Person {
+    String name;
+    String iDCitizen;
+    Date birthday;
+    boolean sex;
+    String phone;
+    String email;
+
+}
