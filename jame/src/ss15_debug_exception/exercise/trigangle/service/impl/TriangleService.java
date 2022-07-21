5304b2f4c079ce749a5b867a368b44f05b31e864
daiphanphuoc <daiphanphuoc@gmail.com>
2022-07-21 19:51:10 +0700

    exception

--- jame/src/ss15_debug_exception/exercise/trigangle/service/impl/TriangleService.java
+++ jame/src/ss15_debug_exception/exercise/trigangle/service/impl/TriangleService.java
@@ -3,6 +3,7 @@ package ss15_debug_exception.exercise.trigangle.service.impl;
 import ss15_debug_exception.exercise.trigangle._exception.IllegalTriangleException;
 import ss15_debug_exception.exercise.trigangle.model.Triangle;
 import ss15_debug_exception.exercise.trigangle.service.IService;
+import ss15_debug_exception.exercise.trigangle.until.Until;
 
 import java.util.Scanner;
 
@@ -17,28 +18,19 @@ public class TriangleService implements IService<Triangle> {
         double c;
 
         while (true) {
-            try {
-                System.out.println("Nhập vào cạnh a: ");
-                a = Double.parseDouble(sc.nextLine());
+            a = Until.getDouble("Nhập vào cạnh a: ");
 
-                System.out.println("Nhập vào cạnh b: ");
-                b = Double.parseDouble(sc.nextLine());
+            b = Until.getDouble("Nhập vào cạnh b: ");
 
-                System.out.println("Nhập vào cạnh c: ");
-                c = Double.parseDouble(sc.nextLine());
-                break;
-            } catch (NumberFormatException e) {
-                System.out.println(e.getMessage());
-                System.out.println("Nhập lại 3 cạnh của tam giác:");
+            c = Until.getDouble("Nhập vào cạnh c: ");
+
+            if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
+                throw new IllegalTriangleException("Không phải 3 cạnh của tam giác, nhập lại.");
             }
+
+            return new Triangle(a, b, c);
+
         }
-
-        if (a <= 0 || b <= 0 || c <= 0 || a + b <= c || a + c <= b || b + c <= a) {
-            throw new IllegalTriangleException("Không phải 3 cạnh của tam giác, nhập lại.");
-        }
-
-        return new Triangle(a, b, c);
-
     }
 
 }
